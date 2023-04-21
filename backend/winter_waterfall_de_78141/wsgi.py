8826474058bf9bfc711241b35c6f581079bcde87
Crowdbotics <engineering@crowdbotics.com>
2023-04-21 01:55:40 +0000

    Initial commit.

---
+++ backend/winter_waterfall_de_78141/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for winter_waterfall_de_78141 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'winter_waterfall_de_78141.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "winter-waterfall-de-78141",
+  url: "https://winter-waterfall-de-78141.botics.co",
+  api: "https://winter-waterfall-de-78141.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
